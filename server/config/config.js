07c7d306821d96948ad42557f49606e3d7164267
Matias <matiasappiani@hotmail.com>
2019-11-29 22:30:19 -0300

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 
+// ============================
+// Vencimiento del token
+// ============================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+// Seed de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
+
 
 // ============================
 // Base de datos
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const { verificaToken } = require('../middlewares/autenticacion');
 
 const app = express();
 
@@ -11,7 +12,7 @@ const app = express();
 
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = Number(req.query.desde) || 0;
 
@@ -76,7 +77,7 @@ app.post('/usuario', function(req, res) {
 });
 
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', verificaToken, function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -101,7 +102,7 @@ app.put('/usuario/:id', function(req, res) {
 });
 
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', verificaToken, function(req, res) {
 
     let id = req.params.id;
 
