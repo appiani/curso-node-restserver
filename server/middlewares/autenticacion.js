f0b8a22d7b9f1903c8bbb34bdcac1dcc969bd501
Matias <matiasappiani@hotmail.com>
2019-12-16 16:46:43 -0300

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 
-// ===================
+// ===========================
 // Verificar Token
-// ===================
+// ===========================
 let verificaToken = (req, res, next) => {
 
     let token = req.get('token');
@@ -25,8 +25,54 @@ let verificaToken = (req, res, next) => {
 
 };
 
+// ===========================
+// Verificar Admin_Role
+// ===========================
+let verificaAdmin_Role = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    }
+};
+
+// ===========================
+// Verificar token para imagen
+// ===========================
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+};
+
 
 
 module.exports = {
-    verificaToken
+    verificaToken,
+    verificaAdmin_Role,
+    verificaTokenImg
 };
